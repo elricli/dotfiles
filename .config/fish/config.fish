f7480eabc96e555f5c23c79f59aa2360638468e2
elricli <elric.li@outlook.com>
2023-03-02 13:01:34 +0800

    Update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,9 +6,12 @@ set -gx EDITOR nvim
 set -gx PATH "$PATH:$HOME/go/bin"
 set -gx PATH "$PATH:$HOME/.emacs.d/bin"
 set -gx PATH "$PATH:$HOME/.cargo/bin"
+set -gx PATH "$PATH:$HOME/.local/bin"
 
-set -gx RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
-set -gx RUSTUP_UPDATE_ROOT https://mirrors.ustc.edu.cn/rust-static/rustup
+#set -gx RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
+#set -gx RUSTUP_UPDATE_ROOT https://mirrors.ustc.edu.cn/rust-static/rustup
+set -gx RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
+set -gx RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
